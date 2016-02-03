79e49ad69c793c18864d1435c4af62a77bf74a78
Joakim Verona <joakim@verona.se>
2016-02-03 12:48:36 +0100

    usermanager added

---
+++ ch6/usermanager/project.clj
@@ -0,0 +1,55 @@
+(defproject usermanager "0.1.0-SNAPSHOT"
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [selmer "0.9.2"]
+                 [com.taoensso/timbre "4.1.2"]
+                 [com.taoensso/tower "3.0.2"]
+                 [markdown-clj "0.9.74"]
+                 [environ "1.0.1"]
+                 [compojure "1.4.0"]
+                 [ring-webjars "0.1.1"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring "1.4.0"
+                  :exclusions [ring/ring-jetty-adapter]]
+                 [metosin/ring-middleware-format "0.6.0"]
+                 [metosin/ring-http-response "0.6.5"]
+                 [bouncer "0.3.3"]
+                 [prone "0.8.2"]
+                 [org.clojure/tools.nrepl "0.2.11"]
+                 [org.webjars/bootstrap "3.3.5"]
+                 [org.webjars/jquery "2.1.4"]
+                 [org.immutant/web "2.1.0"]]
+
+  :min-lein-version "2.0.0"
+  :uberjar-name "usermanager.jar"
+  :jvm-opts ["-server"]
+
+  :main usermanager.core
+
+  :plugins [[lein-environ "1.0.1"]]
+  :profiles
+  {:uberjar {:omit-source true
+             :env {:production true}
+             :aot :all}
+   :dev           [:project/dev :profiles/dev]
+   :test          [:project/test :profiles/test]
+   :project/dev  {:dependencies [[ring/ring-mock "0.3.0"]
+                                 [ring/ring-devel "1.4.0"]
+                                 [pjstadig/humane-test-output "0.7.0"]]
+                  
+                  
+                  :repl-options {:init-ns usermanager.core}
+                  :injections [(require 'pjstadig.humane-test-output)
+                               (pjstadig.humane-test-output/activate!)]
+                  ;;when :nrepl-port is set the application starts the nREPL server on load
+                  :env {:dev        true
+                        :port       3000
+                        :nrepl-port 7000}}
+   :project/test {:env {:test       true
+                        :port       3001
+                        :nrepl-port 7001}}
+   :profiles/dev {}
+   :profiles/test {}})
---
+++ ch6/usermanager/src/usermanager/middleware.clj
@@ -0,0 +1,72 @@
+(ns usermanager.middleware
+  (:require [usermanager.layout :refer [*app-context* error-page]]
+            [taoensso.timbre :as timbre]
+            [environ.core :refer [env]]
+            [selmer.middleware :refer [wrap-error-page]]
+            [prone.middleware :refer [wrap-exceptions]]
+            [ring.middleware.flash :refer [wrap-flash]]
+            [immutant.web.middleware :refer [wrap-session]]
+            [ring.middleware.reload :as reload]
+            [ring.middleware.webjars :refer [wrap-webjars]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
+            [ring.middleware.format :refer [wrap-restful-format]])
+  (:import [javax.servlet ServletContext]))
+
+(defn wrap-context [handler]
+  (fn [request]
+    (binding [*app-context*
+              (if-let [context (:servlet-context request)]
+                ;; If we're not inside a servlet environment
+                ;; (for example when using mock requests), then
+                ;; .getContextPath might not exist
+                (try (.getContextPath ^ServletContext context)
+                     (catch IllegalArgumentException _ context))
+                ;; if the context is not specified in the request
+                ;; we check if one has been specified in the environment
+                ;; instead
+                (:app-context env))]
+      (handler request))))
+
+(defn wrap-internal-error [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Throwable t
+        (timbre/error t)
+        (error-page {:status 500
+                     :title "Something very bad has happened!"
+                     :message "We've dispatched a team of highly trained gnomes to take care of the problem."})))))
+
+(defn wrap-dev [handler]
+  (if (env :dev)
+    (-> handler
+        reload/wrap-reload
+        wrap-error-page
+        wrap-exceptions)
+    handler))
+
+(defn wrap-csrf [handler]
+  (wrap-anti-forgery
+    handler
+    {:error-response
+     (error-page
+       {:status 403
+        :title "Invalid anti-forgery token"})}))
+
+(defn wrap-formats [handler]
+  (wrap-restful-format handler {:formats [:json-kw :transit-json :transit-msgpack]}))
+
+(defn wrap-base [handler]
+  (-> handler
+      wrap-dev
+      wrap-formats
+      wrap-webjars
+      wrap-flash
+      (wrap-session {:cookie-attrs {:http-only true}})
+      (wrap-defaults
+        (-> site-defaults
+            (assoc-in [:security :anti-forgery] false)
+            (dissoc :session)))
+      wrap-context
+      wrap-internal-error))
